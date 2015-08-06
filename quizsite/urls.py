9ae01e14563c0448d2566e1d18191c623002c5b6
saraaven <aven.sara@gmail.com>
2015-08-06 10:13:49 +0200

    skapat URL:er och kopplat till vyer

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -19,3 +19,12 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+from django.conf.urls import url
+from quiz import views
+urlpatterns = [
+	url(“^$”, views.startpage),
+	url(r"^quiz/[a-z-]+/$", views.quiz),
+	url(r"^quiz/[a-z-]+/question/[0-9]/$", views.question),
+	url(r"^quiz/[a-z-]+/completed/$", views.completed),
+]
