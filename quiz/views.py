0dd940cc1d31bbb87b924904cf5a767f7f2a7319
saraaven <aven.sara@gmail.com>
2015-08-06 10:10:55 +0200

    skapat vyer

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def startpage(request):
+	return render(request, "quiz/startpage.html")
+
+def quiz(request):
+	return render(request, "quiz/quiz.html")
+
+def quiz(request):
+	return render(request, "quiz/question.html")
+
+def quiz(request):
+	return render(request, "quiz/completed.html")
+	
