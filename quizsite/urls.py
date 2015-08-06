8a47c63fa34980380fc22f9382c27bea0b3f0492
saraaven <aven.sara@gmail.com>
2015-08-06 19:24:32 +0200

    Tillagd urls

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from quiz import views
 from django.contrib import admin
-from django.conf.urls import include
+from django.conf.urls import include	
 
 urlpatterns = [
 	url("^$", views.startpage, name="start_page"),
 	url(r"^quiz/([a-z-]+)/$", views.quiz, name="quiz_page"),
 	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question, name="question_page"),
 	url(r"^quiz/([a-z-]+)/completed/$", views.completed, name="completed_page"),
+	url(r"^admin/", include(admin.site.urls)),
 ]
