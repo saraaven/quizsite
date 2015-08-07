8fb8781514a4cbb3357963cd57b511e9e4c30788
saraaven <aven.sara@gmail.com>
2015-08-07 10:32:47 +0200

    Gjort ändringar i questions-vyn för att koppla ihop frågorna med databasen
    Så att quizet vet vilken fråga som den ska visa.

--- quiz/views.py
+++ quiz/views.py
@@ -11,19 +11,22 @@ def startpage(request):
 
 def quiz(request, slug):
 	context = {
-			"quiz":quizzes[slug], 
-			"quiz_slug": slug, 
+			"quiz": Quiz.objects.get(slug=slug),  
 	}
 	return render(request, "quiz/quiz.html", context)
 
 def question(request, slug, number):
-	context = {
-		"question_number": number,
-	    	"question": u"Hur många bultar har ölandsbron?",
-		"answer1": u"12",
-	   	"answer2": u"66 400",
-	    	"answer3": u"7 428 954",
-	    	"quiz_slug": slug,
+		number = int(number)
+		quiz = Quiz.objects.get(slug=slug)
+		questions = quiz.questions.all()
+		question = questions[number - 1]
+		context = {
+    			"question_number": number,
+    			"question": question.question,
+	    		"answer1": question.answer1,
+    			"answer2": question.answer2,
+	    		"answer3": question.answer3,
+	    		"quiz": quiz,
 	}
 	return render(request, "quiz/question.html", context)
 
