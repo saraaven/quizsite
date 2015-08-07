88fa54f36529109d2882af279848697dab653e22
saraaven <aven.sara@gmail.com>
2015-08-07 09:52:55 +0200

    Koppla ihop modellerna

--- quiz/views.py
+++ quiz/views.py
@@ -1,24 +1,11 @@
 # coding: utf-8
 from django.shortcuts import render
-
-quizzes = {
-	"klassiker": {
-   		"name": u"Klassiska böcker",
-	   	"description": u"Hur bra kan du dina klassiker?"
-	},
-	"fotboll": {
-	   	"name": u"Största fotbollslagen",
-	   	"description": u"Kan du dina lag?"
-	},
-	"kanda-hackare": {
-	    	"name": u"Världens mest kända hackare",
-	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
-}
+from quiz.models import Quiz
 
 # Create your views here.
 def startpage(request):
 	context = {
-		"quizzes": quizzes,
+		"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/startpage.html", context)
 
